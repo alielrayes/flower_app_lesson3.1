a468db5d33c0af5f2c8065d8d7c78528afa93096
alielrayes <61753691+alielrayes@users.noreply.github.com>
2022-05-17 23:00:37 +0200

    first commit

---
+++ lib/pages/homr.dart
@@ -0,0 +1,100 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
+
+import 'package:flower_app/shared/colors.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: Drawer(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("assets/img/test.jpg"),
+                          fit: BoxFit.cover),
+                    ),
+                    currentAccountPicture: CircleAvatar(
+                        radius: 55,
+                        backgroundImage: AssetImage("assets/img/ali.jpg")),
+                    accountEmail: Text("ali@yahoo.com"),
+                    accountName: Text("ali Hassan",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        )),
+                  ),
+                  ListTile(
+                      title: Text("Home"),
+                      leading: Icon(Icons.home),
+                      onTap: () {}),
+                  ListTile(
+                      title: Text("My products"),
+                      leading: Icon(Icons.add_shopping_cart),
+                      onTap: () {}),
+                  ListTile(
+                      title: Text("About"),
+                      leading: Icon(Icons.help_center),
+                      onTap: () {}),
+                  ListTile(
+                      title: Text("Logout"),
+                      leading: Icon(Icons.exit_to_app),
+                      onTap: () {}),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 12),
+                child: Text("Developed by Ali Hassan © 2022",
+                    style: TextStyle(fontSize: 16)),
+              )
+            ],
+          ),
+        ),
+        appBar: AppBar(
+         
+          actions: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Positioned(
+                      bottom: 24,
+                      child: Container(
+                          child: Text(
+                            "8",
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Color.fromARGB(255, 0, 0, 0)),
+                          ),
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(211, 164, 255, 193),
+                              shape: BoxShape.circle)),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.add_shopping_cart),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 12),
+                  child: Text(
+                    "\$ 13",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ],
+            ),
+          ],
+          backgroundColor: appbarGreen,
+          title: Text("Home"),
+        ));
+  }
+}
